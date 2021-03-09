8191dd56c1b82d7fa859aa1d28e14af2c258d475
nguyenchien789 <52992835+nguyenchien789@users.noreply.github.com>
2021-03-09 16:23:21 +0700

    edit orderdetailmodel

--- Capstone.Project.Data/ViewModels/OrderDetailModel.cs
+++ Capstone.Project.Data/ViewModels/OrderDetailModel.cs
@@ -7,6 +7,6 @@ namespace Capstone.Project.Data.ViewModels
     public class OrderDetailModel
     {
         public int PhotoId { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
     }
 }
